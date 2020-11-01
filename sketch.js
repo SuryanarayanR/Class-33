59edb4e5efbe9c880d879f0bc26eb4c8317983a2
Suryanarayan <suryanarayanrnair@gmail.com>
2020-11-01 18:24:40 +0530

    Debuging final angry birds

--- sketch.js
+++ sketch.js
@@ -6,14 +6,18 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
-var bird, slingshot;
+var bird,bird2,bird3,bird4,slingshot;
 
 var gameState = "onSling";
 var bg = "sprites/bg1.png";
 var score = 0;
+var birds = [];
+var birdfly,birdselect;
 
 function preload() {
     getBackgroundImg();
+    birdfly = loadSound("sprites/bird_flying.mp3");
+    birdselect = loadSound("sprites/bird_select.mp3");
 }
 
 function setup(){
@@ -40,7 +44,15 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(200,50);
+    bird  = new Bird(200,50);
+    bird2 = new Bird(150,170);
+    bird3 = new Bird(100,170);
+    bird4 = new Bird(50,170);
+
+    birds.push(bird4);
+    birds.push(bird3);
+    birds.push(bird2);
+    birds.push(bird);
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
@@ -75,6 +87,10 @@ function draw(){
     log5.display();
 
     bird.display();
+    bird2.display();
+    bird3.display();
+    bird4.display();
+
     platform.display();
     //log6.display();
     slingshot.display();    
@@ -82,19 +98,27 @@ function draw(){
 
 function mouseDragged(){
     if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+        Matter.Body.setPosition(birds[birds.length-1].body, {x: mouseX , y: mouseY});
+        Matter.Body.applyForce(birds[birds.length-1].body,birds[birds.length-1].body.position,{x:10,y:-5});
     }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    birdfly.play();
+    birds.pop();
     gameState = "launched";
 }
 
 function keyPressed(){
-    if(keyCode === 32){
-       // slingshot.attach(bird.body);
+    if(keyCode === 32 && gameState === "launched"){
+        if(birds.length>=0){  
+            Matter.Body.setPosition(birds[birds.length-1].body, {x: 200 , y: 50}); 
+            slingshot.attach(birds[birds.length-1].body);
+            gameState = "onSling"
+            birdselect.play();
+        }
     }
 }
 
